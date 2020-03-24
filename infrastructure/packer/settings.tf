7f3f0bb9e71fb044117538839c5cac67a2fc3ce2
Anton Sidelnikov <a.sidelnikov@t-systems.com>
2020-03-24 15:31:45 +0300

    try cloud.yml

--- infrastructure/packer/settings.tf
+++ infrastructure/packer/settings.tf
@@ -1,14 +1,10 @@
 terraform {
   required_providers {
-    opentelekomcloud = ">= 1.13.1"
+    opentelekomcloud = ">= 1.16.0"
   }
 }
 
 # Configure the OpenTelekomCloud Provider
 provider "opentelekomcloud" {
-  user_name   = var.username
-  password    = var.password
-  domain_name = var.domain_name
-  tenant_name = var.tenant_name
-  auth_url    = "https://iam.eu-de.otc.t-systems.com:443/v3"
+  cloud = "devstack"
 }
